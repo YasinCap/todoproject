b73c8ba32a0f60f7117ab598bea247bfdf4fb508
Yasincoding <yasin52school@gmail.com>
2023-05-15 12:51:04 +0200

    correct

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "todo_app", "static"),
+    os.path.join(BASE_DIR, "todo_project/staticfiles/"),
 ]
 
 # Default primary key field type
